1710baf074cda90fc8878dfdcf5c1409ab9f9982
jordicollell <78138546+jordicollell@users.noreply.github.com>
2022-03-13 09:35:57 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+let strip: neopixel.Strip = null
+let x = 0
+let y = 0
+let z = 0
+basic.forever(function () {
+    strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB_RGB)
+    while (true) {
+        x = input.acceleration(Dimension.X) / 2
+        y = input.acceleration(Dimension.Y) / 2
+        z = input.acceleration(Dimension.Z) / 2
+        strip.shift(1)
+        strip.setPixelColor(0, neopixel.rgb(x, y, 0 - z))
+        strip.show()
+        basic.pause(100)
+    }
+})
